e229a5349c2124b4f588252625926aed9ae439c2
Olena Sovyn <olenasovyn@gmail.com>
2014-12-21 01:34:42 +0200

    Minor fixing + adding functionality

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,6 @@
         $(window).scroll(function() {
             var scroll = $(this).scrollTop();
             var menu = $('.main-menu');
-            console.log('scrolling');
 
             if (scroll >= 100) {
                 menu.removeClass('main-menu_transparent').addClass('main-menu_white');
@@ -15,6 +14,35 @@
                 menu.removeClass('main-menu_white').addClass('main-menu_transparent');
             }
         });
+
+
+        $('.menu').on('click', function() {
+        	$('#menu-button').prop('checked', false);
+        });
+
+        $('.animated').appear(function() {
+            var element = $(this);
+            var animation = element.data('animation');
+            var animationDelay = element.data('delay');
+            if (animationDelay) {
+                setTimeout(function() {
+                    element.addClass(animation + " visible");
+                    element.removeClass('hiding');
+                    if (element.hasClass('counter')) {
+                        element.find('.timer').countTo();
+                    }
+                }, animationDelay);
+            } else {
+                element.addClass(animation + " visible");
+                element.removeClass('hiding');
+                if (element.hasClass('counter')) {
+                    element.find('.timer').countTo();
+                }
+            }
+        }, {
+            accY: -150
+        });
+
 
     });
 })(jQuery);
