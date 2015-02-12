d02ef703db28493d17490dc1b01794e1b1e32f44
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2015-02-12 09:28:21 +0200

    writable

--- bin/mkconfig.py
+++ bin/mkconfig.py
@@ -10,10 +10,10 @@ except ImportError:
 
 def main(db_uri):
     app_yml_config='config/prod/app.yml'
-    with open(app_yml_config) as app_yml:
+    with open(app_yml_config, 'w') as app_yml:
         _app_config = load(app_yml, Loader)
         _app_config['sqlalchemy_engine']['url'] = db_uri
-        app_yml.write(str(dump(_app_config)))
+        dump(_app_config, app_yml)
 
 if __name__ == '__main__':
     main(argv[1])
