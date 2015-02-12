f4f43d04811878eb26a76e3496782228c74696bd
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2015-02-12 02:08:52 +0200

    dump str

--- bin/mkconfig.py
+++ bin/mkconfig.py
@@ -13,7 +13,7 @@ def main(db_uri):
     with open(app_yml_config) as app_yml:
         _app_config = load(app_yml, Loader)
         _app_config['sqlalchemy_engine']['url'] = db_uri
-        dump(_app_config, app_yml)
+        app_yml.write(str(dump(_app_config)))
 
 if __name__ == '__main__':
     main(argv[1])
