de480aac21d1e62f3d311eda4b7ac141d631da5b
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2015-02-12 02:04:32 +0200

    dump fix

--- bin/mkconfig.py
+++ bin/mkconfig.py
@@ -4,16 +4,16 @@ from sys import argv
 
 from yaml import load, dump
 try:
-    from yaml import CLoader as Loader, CDumper as Dumper
+    from yaml import CLoader as Loader
 except ImportError:
-    from yaml import Loader, Dumper
+    from yaml import Loader
 
 def main(db_uri):
     app_yml_config='config/prod/app.yml'
     with open(app_yml_config) as app_yml:
         _app_config = load(app_yml, Loader)
         _app_config['sqlalchemy_engine']['url'] = db_uri
-        app_yml.write(dump(_app_config, Dumper))
+        dump(_app_config, app_yml)
 
 if __name__ == '__main__':
     main(argv[1])
