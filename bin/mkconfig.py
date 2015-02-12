d18ed6fcfe6e9b01946569ddf392fb9944e1bf73
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2015-02-12 02:00:51 +0200

    conf yml write

--- bin/mkconfig.py
+++ bin/mkconfig.py
@@ -2,18 +2,18 @@
 
 from sys import argv
 
-from yaml import load
+from yaml import load, dump
 try:
-    from yaml import CLoader as Loader
+    from yaml import CLoader as Loader, CDumper as Dumper
 except ImportError:
-    from yaml import Loader
+    from yaml import Loader, Dumper
 
 def main(db_uri):
     app_yml_config='config/prod/app.yml'
     with open(app_yml_config) as app_yml:
         _app_config = load(app_yml, Loader)
         _app_config['sqlalchemy_engine']['url'] = db_uri
-        app_yml.write(_app_config)
+        app_yml.write(dump(_app_config, Dumper))
 
 if __name__ == '__main__':
     main(argv[1])
