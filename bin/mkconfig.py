71c50408d9278cb46791c7d87c751da5c71613d0
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2015-02-12 09:33:46 +0200

    file open mode

--- bin/mkconfig.py
+++ bin/mkconfig.py
@@ -10,10 +10,12 @@ except ImportError:
 
 def main(db_uri):
     app_yml_config='config/prod/app.yml'
-    with open(app_yml_config, 'w') as app_yml:
+    with open(app_yml_config, 'r+') as app_yml:
         _app_config = load(app_yml, Loader)
         _app_config['sqlalchemy_engine']['url'] = db_uri
+        app_yml.seek(0)
         dump(_app_config, app_yml)
+        app_yml.truncate()
 
 if __name__ == '__main__':
     main(argv[1])
